c0263b29d9687d3d4701d446d8bb76989eafe30c
Mr.Amir <mr.amirprogrammer@gmail.com>
2018-05-19 15:50:09 +0430

    تغییراتی در نمایش عکس  مکان

--- app/src/main/java/com/example/bayanae/googlemapsb/MapsActivity.java
+++ app/src/main/java/com/example/bayanae/googlemapsb/MapsActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -47,14 +48,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         //تغییر حالت نقشه
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        //افزودن دکمه های زووم به صفحه
+           //افزودن دکمه های زووم به صفحه
         mMap.getUiSettings().setZoomControlsEnabled(true);
         // کمینه زووم
         mMap.setMinZoomPreference(13.3f);
@@ -63,14 +63,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         // Add a marker in Mahabad and move the camera
-        LatLng Mahabad = new LatLng(36.7571092, 45.713091);
-        mMap.addMarker(new MarkerOptions().position(Mahabad).title("Mahabad").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Mahabad, 11));
+        LatLng Mahabad = new LatLng(36.7681269, 45.7210387);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Mahabad, 12.3f));
 
         //makan ek restoran
         LatLng speedar = new LatLng(36.7687816, 45.7217992);
         LatLng maghbareye_bodagh_soltan = new LatLng(36.748550, 45.719686);
-        LatLng estakhre_abshar = new LatLng(36.7567721, 45.717045);
+        LatLng estakhre_abshar = new LatLng(36.756433, 45.719029);
         LatLng bakhi_mikaeil = new LatLng(36.7726177, 45.705645);
         LatLng mahabad_tanakvray = new LatLng(36.781127, 45.729961);
         LatLng daneshgahe_payamenoor = new LatLng(36.7774718, 45.7370912);
@@ -89,7 +88,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
 
-
         //yek class az  modir makan ha misazim
         MarkerOptions markerOptions2 = new MarkerOptions();
         // افزودن عنوان
@@ -100,7 +98,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markerOptions2.position(maghbareye_bodagh_soltan);
         //ba metode zir  ek aks bray icon  an moshakhs  mikonim
         markerOptions2.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
-
 
 
         //yek class az  modir makan ha misazim
@@ -180,8 +177,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         marker7.setTag(7);
 
 
-
-
         //تطبیق نقشه با متد کلیک کردن مکان ها
         mMap.setOnMarkerClickListener(this);
     }
@@ -190,7 +185,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public boolean onMarkerClick(Marker marker) {
 
-        AppCompatImageView location_image ;
+        AppCompatImageView location_image;
         TextView location_about;
 
         //اگر مکانی وجود داشت
@@ -210,47 +205,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             dialog.setTitle(marker.getTitle());
 
 
-
-            if(marker.getTag().equals(1)){
+            if (marker.getTag().equals(1)) {
                 location_image.setImageResource(R.drawable.sepidar);
                 location_about.setText("برگزاری جشن ها و مهمانیهای شما با دیزاین مورد علاقه شما \n" +
                         "رستوران سپیدار\n" +
                         "جهت رزرو :۸۸-۰۴۴۴۲۴۴۶۶۷۷");
 
-            }else if(marker.getTag().equals(2)){
+            } else if (marker.getTag().equals(2)) {
                 location_image.setImageResource(R.drawable.bodagh_soltan);
                 location_about.setText("بداق سلطان یا بداغ سلطان فرزند شیر خان از نوادگان صارم بیگ مکری است که به واسطه آثار ارزشمند تاریخی که از خود به جای گذاشته نام وی به درخشندگی خاصی نسبت به اسلاف و اخلاف خویش دارد. بداق سلطان در سال ۱۰۳۸ ابتدا درمیرآباد ساکن شد و سپس محل امروزی مهاباد را (که در آن زمان ساوجبلاغ نامیده می شد) به عنوان مرکز قلمرو مکری انتخاب کرد");
 
-            }else if(marker.getTag().equals(3)){
+            } else if (marker.getTag().equals(3)) {
                 location_image.setImageResource(R.drawable.abshar);
                 location_about.setText("آدرس:سه راهی بیمارستان،خیابان سرباز جنب پارک سرباز " +
                         "تلفن:04442244005" + "\n" +
                         "روزهای فرد ساعت9تا16:30 سانس خانم هاو ساعت18تا23:15 سانس آقایان" + "\n" +
-                "روز های زوج ساعت 18تا 23:15 سانس آقایان");
+                        "روز های زوج ساعت 18تا 23:15 سانس آقایان");
 
-            }else if(marker.getTag().equals(4)){
+            } else if (marker.getTag().equals(4)) {
                 location_image.setImageResource(R.drawable.baghi_mikaeil);
                 location_about.setText("پارک ملت یا باغ مکائیل مهاباد ، پارکی زیبا و دیدنی ست که در پایین دست سد مهاباد واقع شده، که باشکوه ترین پارک در جنوب آذربایجان غربی محسوب می شه. نور پردازی زیبا در پارک، رودخانه زیبایی که از دل این پارک می گذره با سنگ های بزرگ و جالب داخل آب و منطقه تفریحی و گردشگری که در روبروی پارک و در دل کوه ایجاد شده، همه و همه پارک ملت مهاباد رو به مکانی دیدنی تبدیل کرده");
 
-            }else if(marker.getTag().equals(5)){
+            } else if (marker.getTag().equals(5)) {
                 location_image.setImageResource(R.drawable.tanakora);
                 location_about.setText(" نام سایت : سیستم فروش تاناکورای مهاباد" + "\n" +
                         " آدرس سایت : http://tanakora-mahabad.com" + "\n" +
                         " نوع فعالیت : لوازم خانگی ، تبلیغات و بازاریابی اینترنتی ، فروشگاههای زنجیره ای ، فروشگاه های الکترونیکی \n" +
                         " تاریخ شروع به فعالیت: 1395");
 
-            }else if(marker.getTag().equals(6)){
+            } else if (marker.getTag().equals(6)) {
                 location_image.setImageResource(R.drawable.university_pnu);
                 location_about.setText(" تلفن: 42338211 - 42333856 - 42333852 - 42333854 - 42333600 - 42335070\n" +
                         "دورنگار: 2338222-0444\n" +
                         "پست الكترونيكي: mahpnu8639@yahoo.com\n" +
                         "نشاني: مهاباد ابتدای بلوار شهید شهریکندی - دانشگاه پیام نور - صندوق پستی 438 - کدپستی : 39735-59148\n ");
 
-            }else if(marker.getTag().equals(7)){
+            } else if (marker.getTag().equals(7)) {
                 location_image.setImageResource(R.drawable.shaltotlibrary);
                 location_about.setText("کانال تلگرامی کتابخانه عمومی شیخ شلتوت:" + "\n" +
-                "@shaltotlibrary" + "\n" +
-                "کتابخانه عمومی شیخ شلتوت اولین کتابخانه عمومی شهرستان مهاباد  است که مجموعه\u200Cای غنی و گسترده از منابع گوناگون در زمینه\u200Cهای مختلف علوم، فنون و ادب را در برمی\u200Cگیرد . این کتابخانه دارای 33488نسخه انواع منابع اطلاعاتی است که بیشتر آن کتاب است و از این تعداد 103 نسخه منابع دیداری شنیداری،282 نسخه منابع گویا و 2818 نسخه مربوط به نشریات است.");
+                        "@shaltotlibrary" + "\n" +
+                        "کتابخانه عمومی شیخ شلتوت اولین کتابخانه عمومی شهرستان مهاباد  است که مجموعه\u200Cای غنی و گسترده از منابع گوناگون در زمینه\u200Cهای مختلف علوم، فنون و ادب را در برمی\u200Cگیرد . این کتابخانه دارای 33488نسخه انواع منابع اطلاعاتی است که بیشتر آن کتاب است و از این تعداد 103 نسخه منابع دیداری شنیداری،282 نسخه منابع گویا و 2818 نسخه مربوط به نشریات است.");
             }
 
             //نمایش پنجره
